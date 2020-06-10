2fabeb50ecf357fa0f4b5d98fe25830aef4fc838
leadstar116 <supremedev116@gmail.com>
2020-06-10 21:57:52 +1000

    add prisma and generated

---
+++ src/generated/prisma-client/index.js
@@ -0,0 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var prisma_lib_1 = require("prisma-client-lib");
+var typeDefs = require("./prisma-schema").typeDefs;
+
+var models = [
+  {
+    name: "Link",
+    embedded: false
+  }
+];
+exports.Prisma = prisma_lib_1.makePrismaClientClass({
+  typeDefs,
+  models,
+  endpoint: `https://eu1.prisma.sh/leadstar-7ff2de/hackernews-node/dev`
+});
+exports.prisma = new exports.Prisma();
